3e2756626b2e58873fb7e40835b0e300eef01e29
flopmart <34133163+asadnisar98@users.noreply.github.com>
2024-01-04 12:43:14 +0400

    new

--- myApp.js
+++ myApp.js
@@ -1,9 +1,13 @@
 let express = require('express');
 let app = express();
 
+
 const handlerfunc = (req, res) => {
+
+    app.use("/public", express.static(__dirname + "/public"));
     const absolutePath = __dirname + '/views/index.html'
     res.sendFile(absolutePath);
+
 } 
 
 
