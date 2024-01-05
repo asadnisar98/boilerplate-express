be7fcf75ddedc738b98e61d2c697e84607b59f5f
flopmart <34133163+asadnisar98@users.noreply.github.com>
2024-01-05 12:22:30 +0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -5,11 +5,16 @@ let app = express();
 const handlerfunc = (req, res) => {
 
     app.use("/public", express.static(__dirname + "/public"));
-    const absolutePath = __dirname + '/views/index.html'
+    const absolutePath = __dirname + '/views/index.html';
     res.sendFile(absolutePath);
 
 } 
 
+const jsonhandle = (req, res) => {
+    app.get(res.json({"message": "Hello json"}));
+}
+
+app.get('/json', jsonhandle);
 
 app.get('/', handlerfunc);
 
