e7001ab580729bd98ff6df1889b5b0b021d7d4f7
flopmart <34133163+asadnisar98@users.noreply.github.com>
2024-01-06 15:23:23 +0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,57 +2,23 @@ let express = require('express');
 require('dotenv').config();
 let app = express();
 
-
 const handlerfunc = (req, res) => {
-
     app.use("/public", express.static(__dirname + "/public"));
     const absolutePath = __dirname + '/views/index.html';
     res.sendFile(absolutePath);
-
-} 
+}
 
 const jsonhandle = (req, res) => {
+    let response;
     if (process.env.MESSAGE_STYLE === "uppercase") {
         response = "Hello json".toUpperCase();
-      } else {
+    } else {
         response = "hello json";
-      }
-    app.get(res.json({"message": response}));
+    }
+    res.json({"message": response});
 }
 
 app.get('/json', jsonhandle);
-
 app.get('/', handlerfunc);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
